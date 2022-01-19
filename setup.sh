a7fdf587e214ab1075f0c6cff63ab5dccf800fd2
Janne Mattila <janne.mattila@hotmail.fi>
2022-01-19 14:19:14 +0200

    Update base demo

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ aksName="myaksnetworking"
 acrName="myacrnetworking0000010"
 workspaceName="mynetworkingworkspace"
 vnetName="mynetworking-vnet"
-subnetAks="AksSubnet"
+subnetAks="aks-subnet"
+subnetPods="pod-subnet"
+subnetInternal="internal-subnet"
 identityName="myaksnetworking"
 resourceGroupName="rg-myaksnetworking"
 location="westeurope"
@@ -55,8 +57,21 @@ subnetaksid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $v
   --query id -o tsv)
 echo $subnetaksid
 
-identityid=$(az identity create --name $identityName --resource-group $resourceGroupName --query id -o tsv)
+subnetpodid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $vnetName \
+  --name $subnetPods --address-prefixes 10.3.0.0/24 \
+  --query id -o tsv)
+echo $subnetpodid
+
+subnetinternalid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $vnetName \
+  --name $subnetInternal --address-prefixes 10.4.0.0/24 \
+  --query id -o tsv)
+echo $subnetinternalid
+
+identityjson=$(az identity create --name $identityName --resource-group $resourceGroupName -o json)
+identityid=$(echo $identityjson | jq -r .id)
+identityobjectid=$(echo $identityjson | jq -r .principalId)
 echo $identityid
+echo $identityobjectid
 
 az aks get-versions -l $location -o table
 
@@ -79,15 +94,14 @@ az aks create -g $resourceGroupName -n $aksName \
  --kubernetes-version 1.22.4 \
  --enable-addons monitoring \
  --enable-aad \
- --enable-azure-rbac \
  --enable-managed-identity \
- --enable-pod-identity \
  --disable-local-accounts \
  --aad-admin-group-object-ids $aadAdmingGroup \
  --workspace-resource-id $workspaceid \
  --attach-acr $acrid \
  --load-balancer-sku standard \
  --vnet-subnet-id $subnetaksid \
+ --pod-subnet-id $subnetpodid \
  --assign-identity $identityid \
  --api-server-authorized-ip-ranges $myip \
  -o table
@@ -96,14 +110,16 @@ sudo az aks install-cli
 
 az aks get-credentials -n $aksName -g $resourceGroupName --overwrite-existing
 
-# If using "--enable-azure-rbac" and you need to grant more access rights:
-aksid=$(az aks show -g $resourceGroupName -n $aksName --query id -o tsv)
-az role assignment create \
-  --role "Azure Kubernetes Service RBAC Cluster Admin" \
-  --assignee $aadAdmingGroup \
-  --scope $aksid
-
 kubectl get nodes
+
+# Note: In our setup we want to create load balancer
+# to separate subnet "internal-subnet" and that requires
+# additional access rights to AKS identity:
+az role assignment create \
+  --role "Network Contributor" \
+  --assignee-object-id $identityobjectid \
+  --assignee-principal-type ServicePrincipal \
+  --scope $vnetid
 
 ############################################
 #  _   _      _                      _
@@ -114,25 +130,53 @@ kubectl get nodes
 # Tester web app demo
 ############################################
 
-# Deploy all items from demos namespace
-kubectl apply -f demos/namespace.yaml
-kubectl apply -f demos/deployment.yaml
-kubectl apply -f demos/service.yaml
+# Deploy all items from demos-external namespace
+kubectl apply -f demos-external/namespace.yaml
+kubectl apply -f demos-external/deployment.yaml
+kubectl apply -f demos-external/service.yaml
 
-kubectl get deployment -n demos
-kubectl describe deployment -n demos
+kubectl get deployment -n demos-external
+kubectl describe deployment -n demos-external
 
-pod1=$(kubectl get pod -n demos -o name | head -n 1)
+pod1=$(kubectl get pod -n demos-external -o name | head -n 1)
 echo $pod1
 
-kubectl describe $pod1 -n demos
-kubectl get service -n demos
+kubectl describe $pod1 -n demos-external
+kubectl get service -n demos-external
 
-ingressip=$(kubectl get service -n demos -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+ingressip=$(kubectl get service -n demos-external -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
 echo $ingressip
 
 curl $ingressip
 # -> <html><body>Hello there!</body></html>
+
+# Deploy all items from demos-internal namespace
+kubectl apply -f demos-internal/namespace.yaml
+kubectl apply -f demos-internal/deployment.yaml
+kubectl apply -f demos-internal/service.yaml
+
+kubectl get deployment -n demos-internal
+kubectl describe deployment -n demos-internal
+
+pod2=$(kubectl get pod -n demos-internal -o name | head -n 1)
+echo $pod2
+
+kubectl describe $pod2 -n demos-internal
+kubectl describe service -n demos-internal
+kubectl get service -n demos-internal
+
+ingressip2=$(kubectl get service -n demos-internal -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+echo $ingressip2
+
+curl $ingressip2
+# -> curl: (7) Failed to connect to 10.4.0.4 port 80: No route to host
+
+curl -X POST --data  "HTTP GET \"http://$ingressip2\"" -H "Content-Type: text/plain" "$ingressip/api/commands"
+# -> OK - Access to demos-internal via demo-external app:
+# ---
+# -> Start: HTTP GET "http://10.4.0.4"
+# <html><body>Hello there!</body></html>
+# <- End: HTTP GET "http://10.4.0.4" 418.42ms
 
 # Wipe out the resources
 az group delete --name $resourceGroupName -y
